345731471f60fcdf4529efd723f1e746f26822c6
orphanedcouplet <oliverdayers@gmail.com>
2017-10-06 18:54:19 -0500

    made input-form table, fixed some things/fiddled with others

--- main.py
+++ main.py
@@ -24,12 +24,18 @@ def length_test(input_string):
 
 @app.route("/", methods=["POST"])
 def validate_form():
-    #initialize jinja template
+    #initialize all variables
     template = jinja_env.get_template("input-form.html")
-
-    #validate username
     username = request.form["username"]
     username_error = ""
+    password_initial = request.form["password_initial"]
+    password_initial_error = ""
+    password_verify = request.form["password_verify"]
+    password_verify_error = ""
+    email = request.form["email"]
+    email_error = ""
+
+    #validate username
     if " " in username:
         username_error = "Username must not contain spaces"
         username = ""
@@ -38,26 +44,22 @@ def validate_form():
         username = ""
     
     #validate password_initial
-    password_initial = request.form["password_initial"]
-    password_initial_error = ""
-    if " " in password_initial:
+    if password_initial = username:
+        password_initial_error = "Password must not be the same as username"
+        password_initial = ""
+    elif " " in password_initial:
         password_initial_error = "Password must not contain spaces"
         password_initial = ""
     elif not length_test(password_initial):
         password_initial_error = "Password must be 3-20 characters in length"
         password_initial = ""
-
     #validate password_verify (match)
-    password_verify = request.form["password_verify"]
-    password_verify_error = ""
     #if password_initial != password_verify: "Passwords do not match"
-    if password_initial != password_verify:
+    elif password_initial != password_verify:
         password_verify_error = "Passwords do not match"
         password_verify = ""
 
     #validate email
-    email = request.form["email"]
-    email_error = ""
     #if email = False: fine
     #if email = True and ("@" not in email or "." not in email): "Must provide a valid email address"
     if email and (("@" not in email) or ("." not in email)):
@@ -67,6 +69,8 @@ def validate_form():
     if not username_error and not password_initial_error and not password_verify_error and not email_error:
         return redirect("/registration-successful?username={0}".format(username))
     else:
+        password_initial = ""
+        password_verify = ""
         return template.render(
             username=username, 
             username_error=username_error, 
