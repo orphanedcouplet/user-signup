5856ea5ce16c27d2b9bdfec593ebfaa8ccb7caa9
orphanedcouplet <oliverdayers@gmail.com>
2017-10-06 16:00:40 -0500

    started setting up main.py and input-form.html

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, redirect
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    template = jinja_env.get_template("input-form.html")
+    return template.render()
+
+def length_test(input_string):
+    #if len(input_text) < 3 or len(input_text) > 20: "Must be 3-20 characters in length"
+    if len(input_string) > 20:
+        return False
+    elif len(input_string) < 3:
+        return False
+    else:
+        return True
+
+@app.route("/validate-form", methods=["POST"])
+def validate_form():
+    #validate username
+    username = request.form["username"]
+    username_error = ""
+    if " " in username:
+        username_error = "Username must not contain spaces"
+        username = ""
+    elif not length_test(username):
+        username_error = "Username must be 3-20 characters in length"
+        username = ""
+    
+    #validate password_initial
+    password_initial = request.form["password_initial"]
+    password_initial_error = ""
+    if " " in password_initial:
+        password_initial_error = "Password must not contain spaces"
+        password_initial = ""
+    elif not length_test(password_initial):
+        password_initial_error = "Password must be 3-20 characters in length"
+        password_initial = ""
+
+    #validate password_verify (match)
+    password_verify = request.form["password_verify"]
+    password_verify_error = ""
+        #if password_initial != password_verify: "Passwords do not match"
+    if password_initial != password_verify:
+        password_verify_error = "Passwords do not match"
+        password_verify = ""
+
+    #validate email
+    email = request.form["email"]
+    email_error = ""
+        #if email = False: fine
+    if email:
+        if "@" not in email or "." not in email:
+            email_error = "Must provide a valid email address"
+            email = ""
+        #if email = True and ("@" not in email or "." not in email): "Must provide a valid email address"
+
+app.run()
